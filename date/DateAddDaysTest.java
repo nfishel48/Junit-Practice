1904d40edb59935a92b10bc75c7afdd81ca0e806
Nathan Fishel <nfishel@emich.edu>
2020-11-01 01:23:13 -0500

    work for today

--- date/DateAddDaysTest.java
+++ date/DateAddDaysTest.java
@@ -8,11 +8,15 @@ public class DateAddDaysTest {
     private Date sameMonth;
     private Date diffMonth;
     private Date diffYear;
+    private Date diffMonthTwo;
+    private Date diffMonthThree;
     
     @Before
 	public void setUp() throws Exception {
         sameMonth = new Date("January",3,2019);
         diffMonth = new Date("January",31,2019);
+        diffMonthTwo = new Date("Febuary",28,2019);
+        diffMonthThree = new Date("April",30,2019);
         diffYear = new Date("December",31,2019);
     }
     
@@ -25,7 +29,13 @@ public class DateAddDaysTest {
     @Test
 	public void testDiffMonth() { // Test that month rolls over correctly
         diffMonth.addOneDay();
-		assertEquals(2, sameMonth.getMonth());
-	}
+        diffMonthTwo.addOneDay();
+        diffMonthThree.addOneDay();
+        assertEquals(2, diffMonth.getMonth());
+        assertEquals(3, diffMonthTwo.getMonth());
+        assertEquals(5, diffMonthThree.getMonth());
+    }
+    
+
 }
 
